33dda0593e5a29ae2b14d5895535a7075295b2b7
Paul_Salony <pjsal452@gmail.com>
2020-10-07 15:49:30 -0400

    Defined basic player class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 console.log("JS linked");
 
+class Player {
+    constructor(name, gameOperatorID) {		
+        this.name = name; 									
+        this.gameOperatorID = gameOperatorID;
+        this.boxSelectionsHistory = [];
+        this.winner = false;
+    }
+};
+
 //Find the game grid and game boxes
 const gameGrid = document.querySelector('.game-grid');
 const gameBox = document.querySelectorAll('.box');
 
 // console.log(gameGrid);
 // console.log(gameBox);
+
+//This will determine if a box is open and allow user to select it
+function confirmBoxSelection (currBox) {
+    if (currBox.classList.contains('x') || currBox.classList.contains('o')) {
+        console.log('Box alreday selected');
+    } else {
+        currBox.classList.add('x');
+    }
+
+}
 
 //Add an event listner to the game grid parent and take advantage of propagation 
 gameGrid.addEventListener('click', (event) => {
@@ -16,11 +35,14 @@ gameGrid.addEventListener('click', (event) => {
         //Find out which one was clicked by spinning through box list
         gameBox.forEach((currItem, index) => {
             if (currItem === event.target) {
-                console.log (event);
-                console.log (currItem);
-                console.log("Box that was clicked "+ (index + 1));
-                currItem.classList.add('x');
+                // console.log (event);
+                // console.log (currItem);
+                // console.log("Box that was clicked "+ (index + 1));
+                // currItem.classList.add('x');
+                confirmBoxSelection (currItem);
             }
       })
     }
   });
+
+ const p1 = new Player ('Paul','X');
